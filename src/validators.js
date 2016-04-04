bf6c3443851b91324f736b9b06c43869095398b4
Emil Ahlbäck <e.ahlback@gmail.com>
2016-04-04 14:57:37 +0200

    Export functions instead of consts that are functions

--- src/validators.js
+++ src/validators.js
@@ -7,26 +7,31 @@
 
 const EMAIL_REGEXP = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i; // eslint-disable-line max-len
 
-export const minLength = (value, attribute, model, minimum) =>
-  value && value.length >= minimum;
+export function minLength(value, attribute, model, minimum) {
+  return value && value.length >= minimum;
+}
 
-export const maxLength = (value, attribute, model, maximum) =>
-  value && value.length <= maximum;
+export function maxLength(value, attribute, model, maximum) {
+  return value && value.length <= maximum;
+}
 
-export const isRequired = value =>
-  typeof value === "string" ? value.length > 0 : true &&
-  typeof value !== "undefined" && value !== null;
+export function isRequired(value) {
+  return typeof value === "string" ? value.length > 0 : true &&
+         typeof value !== "undefined" && value !== null;
+}
 
-export const matchesAttribute = (value, attribute, model, other_attribute) =>
-  model[attribute] === model[other_attribute];
+export function matchesAttribute(value, attribute, model, other_attribute) {
+  return model[attribute] === model[other_attribute];
+}
 
-export const matchesRegexp = (value, attribute, model, regexp) =>
-  regexp.test(value);
+export function matchesRegexp(value, attribute, model, regexp) {
+  return regexp.test(value);
+}
 
-export const isEmail = (value, attribute, model) =>
-  matchesRegexp(value, attribute, model, EMAIL_REGEXP);
+export function isEmail(value, attribute, model) {
+  return matchesRegexp(value, attribute, model, EMAIL_REGEXP);
+}
 
-export const isIncluded = (value, attribute, model, list) =>
-  list.indexOf(value) > -1;
-
-
+export function isIncluded(value, attribute, model, list) {
+  return list.indexOf(value) > -1;
+}
